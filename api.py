8f7918a0d0d777a3f214b60bd3859b1612695596
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-01 14:15:58 +0100

    Add files via upload

---
+++ api.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse, PlainTextResponse
+from typing import Optional,List
+from pydantic import BaseModel
+from movie_functions.content import recommendation
+
+
+
+app = FastAPI(
+    title="Recommendation Engine API",
+    description="""An API that recommends movies, anime, music, favourite restaurants using machine learning.""",
+    version="0.0.1", debug=True)
+
+class MovieAPI(BaseModel):
+    movie: str
+
+@app.get("/", response_class=PlainTextResponse)
+async def running():
+  note = """
+Recommendation Engine API 🙌🏻
+
+Note: add "/docs" to the URL to get the Swagger UI Docs or "/redoc"
+  """
+  return note
+
+@app.post("/movie")
+async def movie(data:str):
+    results = recommendation(data)
+    return {"data":results}
