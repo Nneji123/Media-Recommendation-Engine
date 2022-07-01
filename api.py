cc3840b0ead39b261d071e908370c048f7c4b77c
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-01 19:05:43 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,30 +1,54 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, PlainTextResponse
-from typing import Optional,List
+from typing import Optional, List
 from pydantic import BaseModel
-from api_functions.movie_recommender import recommendation
+from api_functions.movie_recommender import recommend_movie
+from api_functions.anime_recommender import recommend_anime
 
 
 app = FastAPI(
     title="Recommendation Engine API",
-    description="""An API that recommends movies, anime, music, favourite restaurants using machine learning.""",
+    description="""An API that utilises machine learning algorithms to recommends movies, anime, music, favourite restaurants using machine learning.""",
     version="0.0.1", debug=True)
 
-# Movie API Route
-class MovieAPI(BaseModel):
-    movie: str
+favicon_path = './images/favicon.png'
+
+
+@app.get('/favicon.png', include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
+
+# Home Page
+
 
 @app.get("/", response_class=PlainTextResponse)
 async def running():
-  note = """
+    note = """
 Recommendation Engine API 🙌🏻
 
 Note: add "/docs" to the URL to get the Swagger UI Docs or "/redoc"
   """
-  return note
+    return note
+
+# Movie API Route
+
+
+class MovieAPI(BaseModel):
+    movie: str
+
 
 @app.post("/movie")
 async def movie(data: MovieAPI):
-    results = recommendation(data.movie)
-    return {"data":results}
+    results = recommend_movie(data.movie)
+    return {"data": results}
 
+
+# Anime Route
+class AnimeAPI(BaseModel):
+    anime: str
+
+
+@app.post("/anime")
+async def anime(data: AnimeAPI):
+    results = recommend_anime(data.anime)
+    return {"data": results}
