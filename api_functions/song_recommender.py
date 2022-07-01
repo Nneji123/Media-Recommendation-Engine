f893df62733b15b0a4eeaa512f747c7311e9c171
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-01 19:07:04 +0000

    working on spotify api

---
+++ api_functions/song_recommender.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.metrics import euclidean_distances
+from scipy.spatial.distance import cdist
+
+import warnings
+warnings.filterwarnings("ignore")
