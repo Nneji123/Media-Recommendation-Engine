32b93b239082fd1bae133d6ca971bd799f05c16a
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-02 09:49:31 +0000

    changes

--- api_functions/book_recommender.py
+++ api_functions/book_recommender.py
@@ -4,26 +4,6 @@ import warnings
 warnings.filterwarnings('ignore')
 from sklearn.metrics.pairwise import cosine_similarity
 
-dfbooks_rating = pd.read_parquet("./data/books.parquet")
-dfbooks = pd.read_parquet("./data/books.parquet")
-dfbooks_rating_count = dfbooks_rating.groupby('User-ID').agg(['count'])['Book-Rating'].reset_index()
-xdfbooks_rating_userID = dfbooks_rating_count[dfbooks_rating_count['count'] > 200] # Count value more than 200
-
-# Filteing the required User of Dataset (Count more than 200 )
-xdfbooks_rating = dfbooks_rating[dfbooks_rating['User-ID'].isin(xdfbooks_rating_userID['User-ID'].tolist() )]
-
-xdfbooks_count = xdfbooks_rating.groupby('Book-Title').agg(['count'])['Book-Rating'].reset_index()
-xdfbooks_popular = xdfbooks_count[xdfbooks_count['count'] >= 50]
-xdfbooks_famous = xdfbooks_rating[xdfbooks_rating['Book-Title'].isin(xdfbooks_popular['Book-Title'].tolist() )].drop_duplicates()
-
-
-xdf_pivot = xdfbooks_famous.pivot_table(index='Book-Title', columns='User-ID', values='Book-Rating')
-xdf_pivot.fillna(0, inplace=True)
-similarity_score = cosine_similarity(xdf_pivot)
-
-# Input the Name of Book While it returns the Similar Book Name
-# with its respective information like Author Name and Image URL
-
 def recommend_book(bk_name):
     # fetch Book Index
     dfbooks_rating = pd.read_parquet("./data/books.parquet")
