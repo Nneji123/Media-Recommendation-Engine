eb4389a999b7874fa59087672fa4085eae9f0ecd
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-02 19:19:55 +0000

    created tests for fastapi routes

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,13 @@
 import json
 from locust import HttpUser, task, between
 from functions.functions import *
+import requests as re
 
 class PerformanceTests(HttpUser):
     wait_time = between(1, 3)
 
     @task(1)
     def API(self):
-        sample = recommend_anime("Death Note")
-        headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
-        self.client.post("/anime", data=json.dumps(sample.dict()), headers=headers)
+        sample = {"game":"Call of Duty"}
+        #headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        test = re.post("http://localhost:8501/games", json=sample)
