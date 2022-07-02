1c91516a3a8e2fd946717c9428582c12479a1f4d
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-02 19:56:07 +0000

    deleted extra files

--- tests/test.py
+++ tests/test.py
@@ -6,7 +6,49 @@ class PerformanceTests(HttpUser):
     wait_time = between(1, 3)
 
     @task(1)
-    def API(self):
+    def testGameAPI(self):
+        value = "Call of Duty"
+        sample = {"game":value}
+        #headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        test = re.post(f"http://localhost:8501/games", json=sample)
+    
+    @task(2)
+    def testMovieAPI(self):
+        value = "Call of Duty"
+        sample = {"game":value}
+        #headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        test = re.post(f"http://localhost:8501/games", json=sample)
+
+        @task(3)
+    def testAnimeAPI(self):
+        value = "Call of Duty"
+        sample = {"game":value}
+        #headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        test = re.post(f"http://localhost:8501/games", json=sample)
+
+        @task(4)
+    def testMangaAPI(self):
+        value = "Call of Duty"
+        sample = {"game":value}
+        #headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        test = re.post(f"http://localhost:8501/games", json=sample)
+
+        @task(5)
+    def testComicAPI(self):
+        value = "Call of Duty"
+        sample = {"game":value}
+        #headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        test = re.post(f"http://localhost:8501/games", json=sample)
+
+        @task(6)
+    def testMusicAPI(self):
+        value = "Call of Duty"
+        sample = {"game":value}
+        #headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        test = re.post(f"http://localhost:8501/games", json=sample)
+
+        @task(7)
+    def testBookAPI(self):
         value = "Call of Duty"
         sample = {"game":value}
         #headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
